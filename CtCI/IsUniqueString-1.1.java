dd7a551b2b89b40fafd0ad788e4f9c823a859eff
Ryan Ting <rting@tcd.ie>
2020-12-11 18:10:03 +0000

    changes in CtCI

--- CtCI/IsUniqueString-1.1.java
+++ CtCI/IsUniqueString-1.1.java
@@ -1,3 +1,16 @@
-public class IsUniqueString-1.1 {
-    
-}
+public static boolean isUnique(String s) {
+
+    HashSet<Character> map = new HashSet<>();
+
+    int i = 0;
+    while(i<s.length()) {
+        if(map.contains(s.charAt(i))) return false;
+        else {
+            map.add(s.charAt(i));
+        }
+        i++;
+    }
+    return true;
+
+
+    }
--- test.java
+++ test.java
@@ -7,26 +7,11 @@ public class test {
 
     public static void main(String[] args) {
 
-        System.out.print(isUnique("ghbndgjso"));
+        
 
     }
 
 
 
-public static boolean isUnique(String s) {
 
-    HashSet<Character> map = new HashSet<>();
-
-    int i = 0;
-    while(i<s.length()) {
-        if(map.contains(s.charAt(i))) return false;
-        else {
-            map.add(s.charAt(i));
-        }
-        i++;
-    }
-    return true;
-
-
-    }
 }
